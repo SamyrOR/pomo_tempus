746241c1f0caf45024ffb56d81488b6c29476fa7
SamyrOR <saamyr@live.com>
2025-03-14 11:20:12 -0300

    feat: added windows notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:local_notifier/local_notifier.dart';
 import 'package:pomo_tempus/theme_handler.dart';
+import 'package:windows_notification/notification_message.dart';
+import 'package:windows_notification/windows_notification.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await localNotifier.setup(
+    appName: 'pomo_tempus',
+    // The parameter shortcutPolicy only works on Windows
+    shortcutPolicy: ShortcutPolicy.requireCreate,
+  );
   runApp(const MyApp());
 }
 
@@ -18,24 +27,7 @@ class MyApp extends StatelessWidget {
       builder: (context, value, child) {
         return MaterialApp(
           title: 'Flutter Demo',
-          theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // TRY THIS: Try running your application with "flutter run". You'll see
-            // the application has a purple toolbar. Then, without quitting the app,
-            // try changing the seedColor in the colorScheme below to Colors.green
-            // and then invoke "hot reload" (save your changes or press the "hot
-            // reload" button in a Flutter-supported IDE, or press "r" if you used
-            // the command line to start the app).
-            //
-            // Notice that the counter didn't reset back to zero; the application
-            // state is not lost during the reload. To reset the state, use hot
-            // restart instead.
-            //
-            // This works for code too, not just values: Most code changes can be
-            // tested with just a hot reload.
-            colorScheme: value,
-          ),
+          theme: ThemeData(colorScheme: value, useMaterial3: true),
           home: const MyHomePage(title: 'Flutter Demo Home Page'),
         );
       },
@@ -46,15 +38,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -63,8 +46,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final themeHandler = ThemeHandler.instance;
+  // LocalNotification notification = LocalNotification(
+  //   title: "local_notifier_example",
+  //   body: "hello flutter!",
+  // );
+
+  final winNotifyPlugin = WindowsNotification(applicationId: 'pomo_tempus');
+  NotificationMessage message = NotificationMessage.fromPluginTemplate(
+    "rest",
+    "",
+    "Focus",
+  );
+
   @override
   Widget build(BuildContext context) {
+    // notification.onShow = () {
+    //   print('onShow ${notification.identifier}');
+    // };
     return Scaffold(
       appBar: AppBar(title: const Text("Flutter")),
       floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -77,7 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
               CupertinoButton(
                 color: Colors.blue,
                 child: const Text("dark blue"),
-                onPressed: () {
+                onPressed: () async {
+                  // notification.show();
+                  await winNotifyPlugin.showNotificationPluginTemplate(message);
                   themeHandler.updateTheme(
                     ColorScheme.fromSeed(seedColor: Colors.blue),
                   );
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  local_notifier
+  windows_notification
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
