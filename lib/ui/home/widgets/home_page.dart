c734d65e3845ee9d443f47de038916964d380ac9
SamyrOR <saamyr@live.com>
2025-03-18 00:10:03 -0300

    feat: added settings dialog and color picker

--- lib/ui/home/widgets/home_page.dart
+++ lib/ui/home/widgets/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,7 +20,9 @@ class _HomePageState extends State<HomePage> {
             child: IconButton(
               icon: Icon(Icons.settings),
               iconSize: 32,
-              onPressed: () {},
+              onPressed: () {
+                _configBuilder(context);
+              },
             ),
           ),
         ],
@@ -55,4 +59,94 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+Future<void> _configBuilder(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Settings"),
+        content: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text("Pomodoro:"),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: "25",
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(suffixText: "min"),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(child: Text("Short break:")),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: "5",
+                        textAlign: TextAlign.center,
+
+                        decoration: InputDecoration(suffixText: "min"),
+
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(child: Text("Long break:")),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: "15",
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(suffixText: "min"),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                BlockPicker(
+                  pickerColor: Colors.amber,
+                  onColorChanged: (color) {
+                    print(color);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("Close"),
+          ),
+        ],
+      );
+    },
+  );
 }
